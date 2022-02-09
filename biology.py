61ab777d9e4c24fb0133295952ac6c40159c4160
IzDmitry <50371867+IzDmitry@users.noreply.github.com>
2022-02-09 22:51:26 +0300

    rework re

--- biology.py
+++ biology.py
@@ -1,43 +1,25 @@
-import time  # тут библиотека времени
-import itertools # генерит ОЧЕНЬ много вариантов
-import re #  регулярочки для поиска 
+import time, itertools, re 
 
-start = time.clock()
-num = input("Типы гамет =  ") # сюда пишем типы гамет
+num = input("\nТипы гамет =  ")
 t = int(len(num) / 2)
 
+start = time.time()
+
 x = sorted(set(list(itertools.combinations(num, t))))
+
 x = ["".join(i) for i in x]
 x = str(x)
-print("Типы гамет = ", num, "\n")
-if t == 2:
-	p = re.compile(r"[Aa][Bb]")
-	m = p.findall(x)
-elif t == 3:
-	p = re.compile(r"[Aa][Bb][Cc]")
-	m = p.findall(x)
-elif t == 4:
-	p = re.compile(r"[Aa][Bb][Cc][Dd]")
-	m = p.findall(x)
-elif t == 5:
-	p = re.compile(r"[Aa][Bb][Cc][Dd][Ee]")
-	m = p.findall(x)
-elif t == 6:
-	p = re.compile(r"[Aa][Bb][Cc][Dd][Ee][Ff]")
-	m = p.findall(x)
-elif t == 7:
-	p = re.compile(r"[Aa][Bb][Cc][Dd][Ee][Ff][Gg]")
-	m = p.findall(x)
-elif t == 8:
-	p = re.compile(r"[Aa][Bb][Cc][Dd][Ee][Ff][Gg][Kk]")
-	m = p.findall(x)	
-elif t == 9:
-	p = re.compile(r"[Aa][Bb][Cc][Dd][Ee][Ff][Gg][Kk][Ll]")
+if t >= 2:
+	p = re.compile(r"[a-zA-Z][a-zA-Z]", re.S)
+	b = p.findall(num)
+	b[:] = ["[" + id + "]"for id in b] 
+	b = "".join(b)
+	p = re.compile(r"" + b)
 	m = p.findall(x)
 else:
-	m = "Цомк"
+	m = "Неверное значение"
 
 c = len(m)
-print(m, "\n\nЧисло типов гамет = ", c, "\n")
-finish = time.clock()
+print("\n", m, "\n\nЧисло типов гамет = ", c, "\n")
+finish = time.time()
 print("Посчитал за = ", finish - start, "сек")
